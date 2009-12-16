3c61ae130a53501944ea8a693859098766b46d9c
root@drupal-dev <root@drupal-dev>
2009-12-16 17:25:38 +0100

    little bugfix

--- modules/os_poker/buddy_list.tpl.php
+++ modules/os_poker/buddy_list.tpl.php
@@ -16,15 +16,21 @@
 		<a href="javascript:void(0);" class="previous">&nbsp;</a>
 		<div class="list">
 			<?php
-				$buddies = $current_user->Buddies(TRUE);
-				$rcount = count($buddies);
-				$limit = $rcount;
-				$placeholder = FALSE;
-				if ($rcount < 7)
+				$limit = 0;
+			
+				if ($current_user)
 				{
-					$limit = 7;
-					$placeholder = TRUE;
+					$buddies = $current_user->Buddies(TRUE);
+					$rcount = count($buddies);
+					$limit = $rcount;
+					$placeholder = FALSE;
+					if ($rcount < 7)
+					{
+						$limit = 7;
+						$placeholder = TRUE;
+					}
 				}
+				
 			?>
 			<div class="cursor">
 				<?php 
@@ -47,7 +53,7 @@
 
 
 		<div id="today_gift">
-		     <?php if ($current_user->CanDailyGift()) { ?>
+		     <?php if ($current_user && $current_user->CanDailyGift()) { ?>
 								<img width="115px" height="165px" style="cursor: pointer;" onclick="javascript:os_poker_send_message({type: 'os_poker_daily_gift'}); this.style.cursor=''; this.src='<?php  print drupal_get_path('theme', 'poker'); ?>/images/banner_invite_more_friends.jpg';" src="<?php  print drupal_get_path('theme', 'poker'); ?>/images/daily_gift.png" alt="Today's free gift"/>
 		     <?php } else { ?>
 			       <img width="112px" height="166px" src="<?php  print drupal_get_path('theme', 'poker'); ?>/images/banner_invite_more_friends.jpg" alt="Today's free gift"/>
