2f2e4b1edb805ba4488ac9a452f67d6063d80918
Pierre Buyle <pierre@buyle.org>
2009-12-18 12:50:41 +0100

    Style header

--- themes/pbpoker/template.php
+++ themes/pbpoker/template.php
@@ -1,44 +1,44 @@
 <?php
 // $Id: template.php $
 
-function poker_theme()
+function pbpoker_theme()
 {
   return array(
 				'user_login_block' => array(
 										'template' => 'user_login',
 										'arguments' => array('form' => NULL),
 									),
-									
+
 				'user_relationships' => array(
 											  'arguments' => array('account' => NULL, 'rtid' => NULL),
 											  'template'  => 'user_relationships',
 											  ),
-											  
+
 				'buddy_block' => array(
 										  'arguments' => array('buddy' => NULL, 'buddyNumber' => NULL, 'hide_links' => FALSE),
 										  'template'  => 'buddy_list_block',
-										  ),	
-										  
+										  ),
+
 				'buddies_tabs' => array(
 											  'arguments' => array('action' => NULL),
 											  'template'  => 'buddies_tabs',
 											  ),
-										  
+
 				'buddies_list' => array(
 											'arguments' => array('buddies' => NULL, 'current_user' => NULL, 'action' => NULL, 'page' => NULL),
 											'template'  => 'buddies_list',
 										),
-										
+
 				'buddies_invite' => array(
 										  'arguments' => array('form' => NULL, 'current_user' => NULL),
 										  'template'  => 'buddies_invite',
 										),
-										
+
 				'buddies_invitedlist' => array(
 										  'arguments' => array('current_user' => NULL),
 										  'template'  => 'buddies_invitedlist',
 										),
-										
+
 				'buddies_search' => array(
 							  'arguments' => array('form' => NULL, 'current_user' => NULL),
 										  'template'  => 'buddies_search',
@@ -93,26 +93,45 @@ function phptemplate_username($object) {
 }
 
 
-function poker_preprocess_user_login_block(&$variables)
+function pbpoker_preprocess_block(&$variables) {
+  $block =& $variables['block'];
+  $classes = $variables['classes'];
+  if($classes) {
+    $classes = explode(' ', $classes);
+  }
+
+  $classes[] = 'block';
+  $classes[] = css_class($block->module).'-block';
+  $classes[] = 'region-'.$variables['block_zebra'];
+  $classes[] = 'region-block-'.$variables['block_id'];
+
+  $variables['classes'] = implode(' ', $classes);
+}
+
+function css_class($string) {
+  return str_replace(array(' ', '_'), '-', $string);
+}
+
+function pbpoker_preprocess_user_login_block(&$variables)
 {
 	$class = "";
 	$attr = & $variables['form']['pass']['#attributes'];
-	
+
 	if (isset($attr) && isset($attr["class"]))
 		$class = $attr["class"];
-		
+
 	$attr["class"] = $class . " custom_input";
 	$variables['form']['pass']['#title'] = '';
-	
+
 	$class = "";
 	$attr = & $variables['form']['name']['#attributes'];
-	
+
 	if (isset($attr) && isset($attr["class"]))
 		$class = $attr["class"];
-		
+
 	$attr["class"] = $class . " custom_input";
 	$variables['form']['name']['#title'] = '';
-	
+
 	$variables['f_name'] = drupal_render($variables['form']['name']);
 	$variables['f_pass'] = drupal_render($variables['form']['pass']);
 	$variables['f_links'] = l(t("Forgot your Password") . "?", "poker/forgot-password", array(	"attributes" => array(
@@ -124,13 +143,13 @@ function poker_preprocess_user_login_block(&$variables)
 																												"width" => "380",
 																										),
 																						));
-	
+
 	if (isset($variables['form']['remember_me']))
 	{
 		$variables['f_remember_me'] = drupal_render($variables['form']['remember_me']);
 		unset($variables['form']['remember_me']);
 	}
-	
+
 	unset($variables['form']['name']);
 	unset($variables['form']['links']);
 	unset($variables['form']['pass']);
