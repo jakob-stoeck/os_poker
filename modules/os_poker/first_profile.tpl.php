3d514cb00a86b14e942dc817dd3d435b187d1934
Pierre Buyle <pierre@buyle.org>
2010-01-15 16:36:02 +0100

    Fix #145

--- modules/os_poker/first_profile.tpl.php
+++ modules/os_poker/first_profile.tpl.php
@@ -3,26 +3,21 @@
 
 <div id="poker-first-profile">
 	<div class="title">
-		<?php print "<h1>" . t("Create your Profile") . "</h1>" . t("One last step to discover the fun, excitement and challenge of poker."); ?>
+		<h1><?php print $title ?></h1> <?php print $subtitle ?>
 	</div>
 	<div class="body">
 		<?php print $form; ?>
 	</div>
 	<div class="find_new_buddies">
 		<p>
-			<strong><?php print t("Find new Poker Buddies!"); ?></strong>
+			<strong><?php print $find_new_buddies ?></strong>
 		</p>
 		<div class="userlist">
-		<?php
-			foreach ($userlist as $uid)
-			{
-				$avatar = "sites/default/files/pictures/picture-".$uid.".png";
-				if (!file_exists($avatar))
-					$avatar = "sites/default/files/pictures/picture-default.png";
-				print "<img class='first_profile_userlist_avatar' src='".$avatar."' />";
-			}
-		?>
+			<?php print $avatars ?>
 		</div>   
+	</div>
+  <div class="footer">
+		<?php print $footer ?>
 	</div>
 </div>
 
