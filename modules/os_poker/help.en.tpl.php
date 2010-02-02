4f80e1f752adf819c33da0a176e4e5ea154f779a
root@drupal-rikeko <root@drupal-rikeko>
2010-02-02 17:14:41 +0100

    starting to import static content for help file, see z2#76

---
+++ modules/os_poker/help.en.tpl.php
@@ -0,0 +1,115 @@
+<div id="ContainerContentHelp">
+    <a href="javascript:void(0);" onclick="javascript:parent.tb_remove();" class="LayerClose">&nbsp;</a>
+	<div class="tabs">
+		<ul class="tabs primary">
+			<li><a href="#">Poker Hands</a></li>
+			<li><a href="#">Hold'Em Rules</a></li>
+			<li><a href="#">Tips</a></li>
+			<li><a href="#">Getting Chips</a></li>
+			<li><a href="#">Guidelines</a></li>
+			<li><a href="#">Security Advice</a></li>
+		</ul>
+	</div>
+
+	<div id="help-rules" class="helpbox">
+		<h4 class="helpbox-title">Hold'em Rules</h4>
+			<p>In Texas Hold’em poker, each player is initially dealt two cards face down (known as hole cards) and five community cards are placed face down in the middle of the table. Hole cards and community cards can be used in any combination with a view to obtaining the best possible five-card hand from the available seven cards. </p>
+			<p><em>Dealer button</em>: The dealer button is the position from which the cards are dealt. Each round of poker begins at the dealer button, which shifts one position to the left after each hand. </p>
+			<p><em>Small blind</em>: Refers to a small bet that the player immediately following the dealer is required to place at the beginning of the first round. </p>
+			<p><em>Big blind</em>: Refers to a large bet that the player immediately following the small blind player is required to place at the beginning of the first round. </p>
+			<ul class="helpbox-list">
+				<li><strong>First round (hole cards) </strong><br />
+  Once each player has been dealt two face-down cards, the player after the big blind player begins the first round, whereby the players can either call, raise, or fold. Each player must place the same chip value in the pot. If a player raises his bet, all other players must either do likewise, pass, or raise the ante. Any player who passes is automatically excluded from the round. </li>
+				<li><strong>Second round (flop cards) </strong><br />
+  Three cards are placed face down as community cards, which all players can use in combination with their two concealed cards. The remaining players now begin the second round, with the player next to the dealer starting the round. Inasmuch as, unlike in the first round, no bet has been placed, this first player has the option to check. All other players can exercise the check option so long as no bet has been placed. To remain in the game, a player must either call or raise. And of course a player can pass at any time, when it is their turn. </li>
+				<li><strong>Third round (turn card) </strong><br />
+  An additional community card known as the turn card is placed face down, with the result that the players now only have six cards available for realization of a five card hand. An additional round now begins, in accordance with the same rules as for the second round. </li>
+				<li><strong>Fourth round (river card) </strong><br />
+				The fifth and final community card, known as the river card, is now placed face down and the final round begins. </li>
+  				<li><strong>Showdown</strong><br />
+The winner with the best five-card hand is now determined from among the remaining players who have not folded. The number of hole cards a player has used has no bearing on whether they win the round. Also see: Poker hand rankings </li>
+			</ul>
+	</div>
+
+	<div id="help-tips" class="helpbox">
+		<h4 class="helpbox-title">Tips</h4>
+		<ol class="helpbox-list">
+			<li><strong>Choose the hands you play wisely</strong><br />
+			Patience is the key to success in poker. Poker professionals only look at the flop in 20 to 30 percent of cases. You will (and should!) increase your chances of getting a good hand by waiting until you get one. </li>
+			<li><strong>There’s nothing wrong with folding</strong><br />
+			Inexperienced players often let themselves be drawn into high bets. In many cases, the chips being played are not commensurate with the anticipated win and the value of your hand. Thus knowing when to fold is one of the keys to successful poker playing. </li>
+			<li><strong>Observe your fellow players carefully</strong><br />
+			You should always pay very close attention to what your fellow players are doing, particularly in cases where you yourself don’t call, because then you obtain a more objective view of what’s going on. Any information you glean from observing your fellow players is valuable. </li>
+			<li><strong>Betting</strong><br />
+			You should only bet when you have a good hand. There are various betting techniques that allow you to obtain information about your fellow players’ hands. For example, doing a re-raise allows you to determine whether or not your fellow players’ bets are merely bluffs.</li>
+			<li><strong>Bluffing</strong><br />
+			Bluffing can potentially be one of the most powerful weapons in the poker player’s arsenal. Used at an opportune moment, bluffing may allow you to win a around; and a failed bluff can induce an opponent to suspect a bluff in a later round and thus induce him to up the ante. However, such risky actions should only be taken in the presence of the right ratio between risk, bets and winnings. </li>
+			<li><strong>Change it up!</strong><br />
+			If you vary the actions you take at the poker table, it will be harder for your opponents to figure out what kind of hand you actually have. </li>
+			<li><strong>Adapt your play to your table position </strong><br />
+			You should take advantage of a good position at the poker table. For example, if you’re the dealer (also known as being “on the button”), you can gather information and make predictions regarding your fellow players’ bets, since you place your bets last. </li>
+			<li><strong>The poker learning curve never ends </strong><br />
+			Since poker rules are easy to learn, even total novices can prevail against highly experienced players. However, it’s also a lot of fun to improve your game based on experience. So the most important poker tip of all is to keep playing.</li>
+		</ol>
+	</div>
+
+	<div id="help-getting-chips" class="helpbox">
+		<h4 class="helpbox-title">Getting Chips</h4>
+		<ul class="helpbox-list">
+			<li><strong>Daily Lottery</strong><br />
+			Your daily chance to win up to 1 Million chips!<br />
+			Just visit the game and you're automatically entered into the daily lottery!</li>
+			<li><strong>Daily bonus chips</strong><br />
+			Obtain free bonus chips every day that you log in.<br />
+			The more buddies you have the more chips you get.</li>
+			<li><strong>Play Poker</strong><br />
+			Win chips at the table und get extra chips for rewards.<br />
+			You can get rich faster by playing higher stakes but of course you can loose it all.</li>
+			<li><strong>Instant Chip Upgrade</strong><br />
+			Finally you can buy or earn chips to upgrade your chip stack hassle-free. </li>
+		</ul>
+	</div>
+
+	<div id="help-guidelines" class="helpbox">
+		<h4 class="helpbox-title">Guidelines</h4>
+		<p>Fairness and mutual respect at the poker table are of supreme importance to us. We ask that you observe the following rules and regulations so as to ensure that all concerned derive maximum enjoyment from playing: </p>
+		<ul class="helpbox-list">
+			<li>Spamming is strictly prohibited, as is posting of affiliate codes and/or links from other Web sites. </li>
+			<li>Posts are to be devoid of any unlawful content. No slander, insults, tirades, obscenity, or racism or any expression of prejudice or discrimination will be tolerated. Any linked or uploaded files such as pictures, audio files or videos are subject to these same rules and are not to violate copyright.</li>
+			<li>Advertising is prohibited.</li>
+			<li>The use of online poker bots is prohibited. Only human players are allowed.</li>
+			<li>Fair play is an absolute must. Any use of software or cheats to gain an unfair advantage over other players is strictly prohibited.</li>
+			<li>Any sale or attempted sale of chips to other players, as well as any purchase of chips from other players, is prohibited. </li>
+		</ul>
+		<p>[Company name] reserves the right to delete any content that violates any of the aforementioned rules and regulations. Any such violation will result in a warning, and in a serious case to immediate deactivation of the relevant user account without prior warning. </p>
+		<p>If you wish to report a rule violation, kindly e-mail us at [support@email.com].</p>
+	</div>
+
+	<div id="help-security" class="helpbox">
+		<h4 class="helpbox-title">Security Advice</h4>
+		<h5>Don’t give Hackers, Spammers and Scammers a chance</h5>
+		<p>We will never ask you for sensitive information via e-mail or in game chat, like</p>
+		<ul>
+			<li>your log-in email</li>
+			<li>your password</li>
+			<li>credit card numbers</li>
+			<li>Don’t follow links to external sources regarding free chips.</li>
+			<li>You should always check the URL in the address bar of your browser when entering personal information</li>
+		</ul>
+		<h5>Keep your password secure</strong></h5>
+		<ul>
+			<li>Never share your password with anyone</li>
+			<li>Change your password at least every six months</li>
+			<li>Never send your password in email, even if the request looks official</li>
+			<li>We recommend a password that is at least 8 characters in length. Each character you add to your password increases the protection it provides.</li>
+			<li>A strong password contains uppercase and lowercase letters, numbers, and special characters</li>
+		</ul>
+		<h5>Avoid weak and easy-to-guess passwords</h5>
+		<ul>
+			<li>No sequences or repeated characters</li>
+			<li>Don't use your login name</li>
+			<li>No dictionary words in any language</li>
+			<li>Don't use only one password for all your accounts</li>
+		</ul>
+	</div>
+</div>
