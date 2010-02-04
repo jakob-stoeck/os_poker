8469c15a0b44f9939e5381b1a4526bdc4d6a2d2b
Pierre Buyle <pierre@buyle.org>
2010-02-04 16:01:50 +0100

    Change background image for welcome spash.

--- themes/pbpoker/pbpoker.js
+++
@@ -1,3 +0,0 @@
-Drupal.theme.os_poker_popup = function(id) {
-  return '<div id="'+id+'" class="messages-popup"><a class="close" href="#">close</a><div class="content"></div><a class="button close">'+Drupal.t('OK')+'</a></div>';
-}
