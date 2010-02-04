53ebc61ddaea070bac1b32186511d8cd21e03b9d
Ulrike Uhlig <u@curlybracket.net>
2010-02-04 23:38:01 +0100

    added german static content see z2#76

---
+++ modules/os_poker/help.de.tpl.php
@@ -0,0 +1,161 @@
+<div id="ContainerContentHelp">
+	<a href="javascript:void(0);" onclick="javascript:parent.tb_remove();" class="LayerClose">&nbsp;</a>
+	<div class="tabs">
+		<ul class="tabs primary">
+			<li><a href="#">Pokerblätter</a></li>
+			<li><a href="#">Hold'Em Regeln</a></li>
+			<li><a href="#">Tipps</a></li>
+			<li><a href="#">Chips kaufen</a></li>
+			<li><a href="#">Richtlinien</a></li>
+			<li><a href="#">Sicherheitshinweis</a></li>
+		</ul>
+	</div>
+
+
+	<div id="help-pokerhands" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Pokerblätter</h4>
+		<dl class="helpbox-list">
+			<dt>Royal Flush
+				<span class="poker-hand royal-flush" title="Royal Flush">A,K,Q,J,10</span>
+			</dt>
+			<dd>Ein Royal Flush ist ein Straight Flush mit einem Ass als höchstem Kartenwert: Zehn, Bube, Dame, König, Ass – alle Karten in Pik oder alle Karten in Kreuz usw.</dt>
+			<dt>Straight Flush
+				<span class="poker-hand straight-flush" title="Straight Flush">4,5,6,7,8</span>
+			</dt>
+			<dd>Ein Straight Flush ist eine Kombination aus Flush und Straße. Somit sind alle Karten von derselben Farbe und aufeinander folgend. Der Rang mehrerer Straßen untereinander richtet sich nach dem Wert der jeweils höchsten Karte der Straße. 87654 wird von 98765 geschlagen. Die Farbe hat keinen Einfluss auf den Wert.</dd>
+    		<dt>Quads / Poker / Vierling
+				<span class="poker-hand four-of-a-kind" title="Quads/Poker/Vierlinf">Q,Q,Q,Q</span>
+			</dt>
+			<dd>Alle vier Karten sind von gleichem Wert, wobei der Rang sich nach der Höhe des Wertes richtet. </dd>
+			<dt>Full House
+				<span class="poker-hand full-house" title="Full House">A,A,A,4,4</span>
+			</dt>
+			<dd>Ein Full House ist eine Kombination aus einem Drilling und einem Paar. Also 3 Karten von gleichem Wert plus 2 Karten von gleichem Wert. Ein Gleichstand bei einem Full House wird nach dem Wert der Drillinge entschieden.</dd>
+			<dt>Flush
+				<span class="poker-hand flush" title="Flush">K,D,10,4,2</span>
+			</dt>
+			<dd>Fünf Karten einer Farbe. Bei einem Gleichstand zählt der Wert der höchsten Poker Karte. Sollten also z.B. die ersten vier Karten den gleichen Wert aufweisen, zählt die höchste fünfte. Sind alle fünf Karten gleich, wird der Pot aufgeteilt, da die Farben gleichwertig sind.</dd>
+			<dt>Strasse
+				<span class="poker-hand straight" title="Strasse">10,J,Q,K,A</span>
+			</dt>
+			<dd>Eine Straße ist ein Blatt, bei dem alle Karten aufeinander folgen (Staight), jedoch nicht alle Karten von einer Farbe sind. Die Straße, die auf der höchsten Karte endet, gewinnt. Bei Gleichheit gilt ein Unentschieden. Der Pot wird geteilt. Bei einer Straße kann das Ass entweder als Ass oder als 1 zählen. Daraus ergibt sich die von ihrer Wertigkeit niedrigste Straße (A, 2, 3, 4, 5) und die höchste mit (10, J, Q, K, A). </dd>
+			<dt>Drilling
+				<span class="poker-hand three-of-a-kind" title="Drilling">K,K,K</span>
+			</dt>
+			<dd>Drei Karten gleichen Wertes. Der höchste Drilling gewinnt bei Drilling gegen Drilling. </dd>
+			<dt>Zwei Paare
+				<span class="poker-hand two-pairs" title="2 Paare">A,A,9,9</span>
+			</dt>
+			<dd>Der Spieler mit dem höchsten Paar gewinnt wenn mehrere Spieler zwei Paare halten. Wenn auch das höchste Paar gleich ist, zählt das zweithöchste. Sollte auch dieses gleich sein, zählt die fünfte Karte als so genannter Kicker.</dd>
+			<dt>Paar
+				<span class="poker-hand one-pair" title="Paar">A,A</span>
+			</dt>
+		    <dd>Zwei Karten gleichen Wertes. Der Spieler mit dem höchsten Paar gewinnt, wenn mehrere Spieler ein Paar halten.</dd>
+			<dt>Höchst Karte
+				<span class="poker-hand high-card" title="Höchste Karte">A</span>
+			</dt>
+			<dd>Die höchste Poker-Karte gewinnt, wenn kein Spieler ein Paar oder ein höheres Blatt hält.</dd>
+		</dl>
+	</div>
+
+	<div id="help-rules" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Hold'em Regeln</h4>
+			<p><em>Small Blind:</em> Kleiner Pflichteinsatz den der auf den Dealer folgende Spieler zu Beginn der ersten Wettrunde zu zahlen hat.</p>
+			<p><em>Big Blind:</em> Grosser Pflichteinsatz den der auf den Small Blind folgende Spieler zu Beginn der ersten Wettrunde zu zahlen hat.</p>
+In Texas Hold’em poker, each player is initially dealt two cards face down (known as hole cards) and five community cards are placed face down in the middle of the table. Hole cards and community cards can be used in any combination with a view to obtaining the best possible five-card hand from the available seven cards. </p>
+			<ul class="helpbox-list">
+				<li><strong>Erste Wettrunde – Hole Cards</strong><br />
+				Nachdem jeder Spieler 2 verdeckte Karten erhalten hat, beginnt der nächste Spieler nach dem Big Blind die 1. Wettrunde. Die Spieler haben folgende Möglichkeiten: Mitgehen (call), Erhöhen (raise) oder Passen (fold). Jeder Spieler muss denselben Betrag an Chips in den Pot zahlen. Erhöht ein Spieler den Einsatz müssen folglich die anderen Spieler entweder mitgehen oder passen oder wiederum den Einsatz erhöhen. Ein Spieler der passt, scheidet aus der Runde aus.</li>
+				<li><strong>Zweite Wettrunde - Flop</strong><br />
+				Drei Karten werden als Gemeinschaftskarten aufgedeckt. Jeder Spieler kann diese in Kombination mit seinen beiden verdeckten Karten verwenden. Die verbliebenen Spieler beginnen nun mit der 2. Wettrunde. Der auf den Dealer folgende Spieler beginnt. Da im Gegensatz zur 1. Runde noch kein Einsatz erbracht wurde, hat er die Option zu schieben. (checken). Alle folgenden Spieler können solange checken wie kein Einsatz erbracht ist. Setzt ein Spieler so bleiben Mitgehen und Erhöhen als Option um im Spiel zu bleiben. Natürlich kann der Spieler der an der Reihe ist jederzeit passen.</li>
+				<li><strong>Dritte Wettrunde - Turn</strong><br />
+				Eine weitere Gemeinschaftskarte, der Turn, wird aufgedeckt sodass den Spielern nun insgesamt 6 Karten zur Verfügung stehen um eine Kombination aus 5 Karten zu bilden. Eine weitere Wettrunde beginnt nach denselben Regeln wie in Runde 2.</li>
+				<li><strong>Vierte Wettrunde – River</strong><br />
+				Nun folgt die 5. und letzte Gemeinschaftskarte, der River. Die letzte Wettrunde beginnt.</li>
+  				<li><strong>Showdown</strong><br />
+				Unter den verbleibenden Spielern, die nicht gefoldet haben wird der Gewinner mit dem besten Blatt aus 5 Karten ermittelt. Dabei spielt es keine Rolle wie viele Karten er von seinen hole cards dafür verwendet hat.</li>
+			</ul>
+	</div>
+
+	<div id="help-tips" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Tipps</h4>
+		<ol class="helpbox-list">
+			<li><strong>Spiele ausgewählte Hände</strong><br />
+			Geduld führt zu erfolgreichem Spiel. Poker-Profis schauen sich nur in 20-30% der Fälle den Flop an. Es lohnt sich, auf gute Starthände zu warten, um den Glücksfaktor zu minimieren.</li>
+			<li><strong>Folden ist keine Schande</strong><br />
+			Als Anfänger lässt man sich viel zu oft in hohe Einsätze herein ziehen. Oft entsprechen die zu bringenden Chips in keiner Weise dem zu erwartenden Gewinn und der Wertigkeit der eigenen Hand. Zu wissen, wann man foldet, ist einer der Schlüssel zu einem guten Spiel.</li>
+			<li><strong>Beobachte deine Mitspieler</strong><br />
+			Du solltest das Spiel Deiner Gegner immer aufmerksam verfolgen, besonders wenn Du selber nicht mitgehst, da dadurch eine objektivere Einschätzung möglich ist. Jede Information, die man aus der Beobachtung seiner Gegenspieler sammeln kann, ist wertvoll.</li>
+			<li><strong>Die Kunst des Setzens</strong><br />
+			Man setzt nicht nur, wenn man eine gute Hand hat. Es gibt verschiedene Techniken des Setzens, um an Informationen über seine Gegenspieler zu gelangen. Mit einem re-raise (nochmaliges Erhöhen) zum Beispiel kannst Du die Glaubwürdigkeit des Einsatzes eines Mitspielers testen.</li>
+			<li><strong>Der Bluff</strong><br />
+			Ein Bluff kann einer der mächtigsten Spielzüge im Poker sein. Zur richtigen Zeit eingesetzt, kann man durch Bluffs nicht nur einzelne Runden für sich entscheiden, auch ein fehlgeschlagener Bluff kann den Gegner dazu bringen, bei späteren Runden auch wieder einen Bluff zu vermuten, und ihn so zu höheren Einsätzen ermutigen. Jedoch sollte bei solch risikoreichen Aktionen immer das Verhältnis von Risiko, Einsatz und Gewinn stimmen.</li>
+			<li><strong>Variiere deine Speilweise</strong><br />
+			Wenn Du Deine Spielweise häufiger wechselst, bleibst Du unberechenbar für Deine Gegenspieler. Es ist schwieriger die Stärke deiner Hand zu deuten.</li>
+			<li><strong>Passe dein Spiel deiner Position am Tisch an</strong><br />
+			Es ist von Vorteil eine gute Position am Tisch zu nutzen. Bist Du zum Beispiel "on the Button", also der Dealer, kannst Du Informationen und Einschätzungen aus der Verhaltensweise deiner Gegner sammeln, da du als letzter in der Setzrunde Deine Entscheidung treffen musst.</li>
+			<li><strong>Man lernt nie aus</strong><br />
+			Die Poker-Regeln sind schnell gelernt und Spaß macht es von Anfang an, da auch der absolute Anfänger gegen einen Profi gewinnen kann. Spannend bleibt es aber immer, da man beim Poker durch Erfahrung ständig sein Spiel verbessert. Daher der beste Tipp: spielen, spielen, spielen!</li>
+		</ol>
+	</div>
+
+	<div id="help-getting-chips" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Getting Chips</h4>
+		<ul class="helpbox-list">
+			<li><strong>Daily Lottery</strong><br />
+			Jeden Tag hast du die Chance bis zu 1 Million Chips zu gewinnen.<br />
+			Logge dich einfach täglich ein und du nimmst automatisch an der Lotterie teil!</li>
+			<li><strong>Daily bonus chips</strong><br />
+			Für jeden Tag an dem du dich einloggst erhältst du kostenlose Chips geschenkt.<br />
+			Je mehr Buddies du hast desto mehr Chips erhältst du als Bonus.</li>
+			<li><strong>Play Poker</strong><br />
+			Gewinne Chips beim Pokern und verdiene dir zusätzliche Chips durch Rewards.<br />
+Spiele an Tischen mit höheren Einsätzen um schneller “reich” zu werden aber denke daran, dass du ebenso schnell alles verlieren kannst.</li>
+			<li><strong>Instant Chip Upgrade</strong><br />
+			Zusätzlich kannst du ganz einfach Chips kaufen oder verdienen um sofort und mühelos deine Bankroll aufzustocken.</li>
+		</ul>
+	</div>
+
+	<div id="help-guidelines" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Richtlinien</h4>
+		<p>Fairness am Spieltisch und gegenseitiger Respekt sind unsere wichtigsten Grundsätze. Um den Spaß am Spiel zu gewährleisten bitten wir euch folgende Regeln zu beachten:</p>
+		<ul class="helpbox-list">
+			<li>Kein Spamming! Bitte keine Veröffentlichung von Affiliate Codes und/oder Links zu anderen Websites.</li>
+			<li>Posts dürfen keinen gesetzwidrigen Inhalt haben. Wir dulden keine Diffamierungen, Beschimpfungen, Beleidigungen, obszöne Äußerungen, rassistische oder diskriminierende Äußerungen. Verlinkte oder hochgeladene Dateien, wie Bilder, Audio-Dateien oder Videos unterliegen denselben Regeln und dürfen kein Copyright verletzen.</li>
+			<li>Werbung ist nicht gestattet.</li>
+			<li>Der Einsatz von Bots am Spieltisch ist nicht gestattet. Nur menschliche Spieler sind erlaubt.</li>
+			<li>Bleibt fair! Jeder Einsatz von Software oder Cheats um sich einen unfairen Vorteil gegenüber anderen Spielern zu verschaffen ist nicht gestattet.</li>
+			<li>Das Verkaufen oder der Versuch des Verkaufens von Chips an andere Spieler ist nicht gestattet. Ebenso ist das Kauf en von Chips von anderen Spielern nicht gestattet.</li>
+		</ul>
+		<p>[Firmenname] behält sich vor, jeglichen Inhalt, welcher gegen die oben genannten Regeln verstößt ohne Vorwarnung zu löschen. Verstöße gegen diese Regeln führen zu einer Verwarnung und in schweren Fällen zu sofortiger Deaktivierung des User-Accounts ohne Vorwarnung.</p>
+		<p>Wer einen Regelverstoß melden möchte sendet bitte eine E-Mail an den Support: [support@email.com].</p>
+	</div>
+
+	<div id="help-security" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Sicherheitshinweis</h4>
+		<h5>Gib Hackern, Spammern und Betrügern keine Chance</h5>
+		<p>Wir werden dich niemals per E-Mail oder im Spieler-Chat nach vertraulichen Informationen fragen, wie</p>
+		<ul>
+			<li>Deiner Login E-Mail</li>
+			<li>Deinem Passwort</li>
+			<li>Deiner Kreditkarten-Nummer</li>
+			<li>Ignoriere Links zu externen Seiten die dir kostenlose Chips versprechen.</li>
+			<li>Du solltest immer die URL in der Adresszeile deines Browsers überprüfen bevor du auf einer Seite persönliche Informationen eingibst.</li>
+		</ul>
+		<h5>Passwort-Sicherheit</h5>
+		<ul>
+			<li>Verrate dein Passwort grundsätzlich niemandem</li>
+			<li>Ändere dein Passwort mindestens alle 6 Monate</li>
+			<li>Sende dein Passwort niemals per E-Mail, selbst wenn die Anfrage offiziell erscheint</li>
+			<li>Wir empfehlen ein Passwort, das mindestens aus 8 Zeichen besteht. Jedes Zeichen, das du deinem Passwort hinzufügst, verbessert die Sicherheit deines Passwortes erheblich.</li>
+			<li>Ein sicheres Passwort enthält Groß- und Kleinbuchstaben, Zahlen und Sonderzeichen.</li>
+		</ul>
+		<h5>Vermeide schwache und leicht zu erratende Passwörter</h5>
+		<ul>
+			<li>Keine aufeinander folgenden oder sich wiederholenden Zeichen</li>
+			<li>Benutze nicht deinen Login Namen als Passwort</li>
+			<li>Keine Wörter gleich welcher Sprache, die in Wörterbüchern zu finden sind</li>
+			<li>Benutze nicht dasselbe Passwort wie bei anderen Accounts</li>
+		</ul>
+	</div>
+</div>
