87b2866feb674da86a6d923f93474eddafdbf978
root@drupal-chandan <root@drupal-chandan>
2010-02-04 09:44:30 +0100

    Use Dupal.behavior instead of  for z2#50

--- modules/os_poker/js/os_poker.messageboxreset.js
+++ modules/os_poker/js/os_poker.messageboxreset.js
@@ -1,17 +1,14 @@
 /*
  * this script is included in the messagebox iframe, and resets the unread message count in the navbar.
  */ 
-function os_poker_reset_messagebox() {
+
+Drupal.behaviors.mesageboxResetBehavior = function() {
 	var mbox_container = $("#block-menu-menu-messages-links a", window.top.document);
 	
 	var pix = mbox_container.find("#mbox_pix");										
 	var count = mbox_container.find("#mbox_count");						
 	count.text('');
 	pix.hide();
-}
+};
 
-
-$(window).ready(function() {
-	os_poker_reset_messagebox();
-});
 
