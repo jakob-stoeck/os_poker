5b05b647190d9ec616aecbbd093b4972ee1d8209
Pierre Buyle <pierre@buyle.org>
2010-02-09 12:26:36 +0100

    Update english toturial and add German tutorial, fixes z2#63.

--- themes/pbpoker/template.php
+++ themes/pbpoker/template.php
@@ -94,16 +94,17 @@ function pbpoker_preprocess_os_poker_teaser(&$variables) {
 
 function pbpoker_poker_tutorial_link() {
   $tutorial = pbpoker_flash_tutorial();
-  $url = '#TB_inline?height='. ($tutorial['size'][1]+5) .'&width='. ($tutorial['size'][0]) .'&inlineId=poker-tutorial';
-  return l(t("Click here!"), $url, array(
+  return l(t("Click here!"), '#TB_inline', array(
     'external' => TRUE,
     'attributes' => array(
       'class' => 'yellow thickbox',
     ),
+    'query' => 'height='. ($tutorial['size'][1]+5) .'&width='. ($tutorial['size'][0]) .'&inlineId=poker-tutorial&',
   ));
 }
 
 function pbpoker_flash_tutorial() {
+  global $language;
   static $file, $size;
   if(!isset($file)) {
       $file = drupal_get_path('theme', 'pbpoker') .'/swf/PokerTutorial.'. $language->language .'.swf';
