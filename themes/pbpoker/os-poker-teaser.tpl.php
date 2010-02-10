a4f46bd26438a371df7b998e7f735532587d72b5
root@drupal-demo <root@drupal-demo>
2010-02-10 07:27:52 +0100

    Implement close cross for z2#89

--- themes/pbpoker/os-poker-teaser.tpl.php
+++ themes/pbpoker/os-poker-teaser.tpl.php
@@ -9,6 +9,7 @@
   <?php print $girl;?>
   <div id="poker-tutorial">
     <div class="tb-fix-margin">
+<a href="javascript:void(0);" onclick="javascript:parent.tb_remove();" class="LayerClose"> </a>
       <script src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js" type="text/javascript"></script>
       <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="<?php print $tutorial['size'][0]?>" height="<?php print $tutorial['size'][1]?>">
         <param name="movie" value="<?php print $tutorial['file']?>" />
