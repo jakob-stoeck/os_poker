6a492c234c9895e5e7f7760b4a505bb673345f89
root@drupal-chandan <root@drupal-chandan>
2010-02-12 00:49:59 +0100

    fixes z2#155

--- modules/os_poker/shop_tabs.tpl.php
+++ modules/os_poker/shop_tabs.tpl.php
@@ -4,7 +4,6 @@
 		<ul class="tabs primary">
 		<li <?php if ($active_tab == "shop" || $active_tab == NULL) { print 'class="active"'; } ?>><?php print l(t("Shop"), "poker/shop"); ?></li>
 		<li <?php if ($active_tab == "get_chips") { print 'class="active"'; } ?>><?php print l(t("Get Chips"), "poker/shop/get_chips"); ?></li>
-		<li <?php if ($active_tab == "earn_chips") { print 'class="active"'; } ?>><?php print l(t("Earn Chips"), "poker/shop/earn_chips"); ?></li>
 		</ul>
 	</div>
 	<div class="content">
