3e7efca4e4f439d42f2ca2b5ae241da4e830f4fc
loic@dachary.org <loic@dachary.org>
2010-02-11 19:47:51 +0100

    typo in link

--- modules/os_poker/news.tpl.php
+++ modules/os_poker/news.tpl.php
@@ -4,7 +4,7 @@
 		<div class="list splash">
 			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="100">
 				<param name="movie" value="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/poker_300x100.swf" />
-                                <param name="FlashVars" value="clickTag=hhttp://www.bunnystore.de/playboy/" />
+                                <param name="FlashVars" value="clickTag=http://www.bunnystore.de/playboy/" />
 				<!--[if !IE]>-->
 				<object FlashVars="clickTag=http://www.bunnystore.de/playboy/" type="application/x-shockwave-flash" data="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/poker_300x100.swf" width="300" height="100">
 				<!--<![endif]-->
