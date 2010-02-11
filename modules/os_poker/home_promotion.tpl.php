c470436cfc641a2319189850e8a990011115d1be
root@drupal-demo <root@drupal-demo>
2010-02-11 17:38:38 +0100

    Refs z2#154. Fix link to http://www.bunnystore.de/playboy/. Hide all <object> when there is an overlay to avoid Flash element over the overlay.

--- modules/os_poker/home_promotion.tpl.php
+++ modules/os_poker/home_promotion.tpl.php
@@ -4,8 +4,9 @@
 	<div class="list splash">
       <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="100">
         <param name="movie" value="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/shop_300x100.swf" />
+        <param name="FlashVars" value="clickTag=http://www.bunnystore.de/playboy/" />
         <!--[if !IE]>-->
-        <object type="application/x-shockwave-flash" data="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/shop_300x100.swf" width="300" height="100">
+        <object FlashVars="clickTag=http://www.bunnystore.de/playboy/" type="application/x-shockwave-flash" data="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/shop_300x100.swf" width="300" height="100">
         <!--<![endif]-->
           <p></p>
         <!--[if !IE]>-->
