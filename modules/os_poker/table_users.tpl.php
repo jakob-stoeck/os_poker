ed7c5a30067776c0130bacc2b70be78b57c389c4
Ulrike Uhlig <u@curlybracket.net>
2010-02-11 16:19:47 +0100

    bad filename see z2#154

--- modules/os_poker/table_users.tpl.php
+++ modules/os_poker/table_users.tpl.php
@@ -6,9 +6,9 @@
 		<div class="previous"></div>
 		<div class="list splash">
 			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="250">
-				<param name="movie" value="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/shop_300x250.swf" />
+				<param name="movie" value="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/poker_300x250.swf" />
 				<!--[if !IE]>-->
-				<object type="application/x-shockwave-flash" data="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/shop_300x250.swf" width="300" height="250">
+				<object type="application/x-shockwave-flash" data="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/poker_300x250.swf" width="300" height="250">
 				<!--<![endif]-->
 				<p></p>
 				<!--[if !IE]>-->
