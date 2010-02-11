b4a35a55aa5805f148fd645c055a12a341fbd95e
loic@dachary.org <loic@dachary.org>
2010-02-11 19:28:10 +0100

    change banners to swf files

--- modules/os_poker/home_promotion.tpl.php
+++ modules/os_poker/home_promotion.tpl.php
@@ -1,25 +1,33 @@
 <div id="home_promotion_2">
-	<div class="panel">
-		<div class="previous"></div>
-		<div class="list splash">
-
-<!-- load block + js ? -->
-
-		</div>
-		<div class="next"></div>
-		<div class="clear"></div>
+<div class="panel">
+	<div class="previous"></div>
+	<div class="list splash">
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="100">
+        <param name="movie" value="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/shop_300x100.swf" />
+        <param name="FlashVars" value="clickTag=http://www.bunnystore.de/playboy/" />
+        <!--[if !IE]>-->
+        <object FlashVars="clickTag=http://www.bunnystore.de/playboy/" type="application/x-shockwave-flash" data="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/shop_300x100.swf" width="300" height="100">
+        <!--<![endif]-->
+          <p></p>
+        <!--[if !IE]>-->
+        </object>
+        <!--<![endif]-->
+      </object>
 	</div>
+	<div class="next"></div>
+	<div class="clear"></div>
+</div>
 </div>
 
 
 <!--
 <div id="home_promotion">
-	<div class="previous fleft">&nbsp;</div>
-	<div class="middle fleft">
-		<div class="promotion">
-		</div>
+<div class="previous fleft">&nbsp;</div>
+<div class="middle fleft">
+	<div class="promotion">
 	</div>
-	<div class="next fleft">&nbsp;</div>
-	<div class="clear"></div>
+</div>
+<div class="next fleft">&nbsp;</div>
+<div class="clear"></div>
 </div>
 -->
--- modules/os_poker/news.tpl.php
+++ modules/os_poker/news.tpl.php
@@ -2,10 +2,29 @@
 	<div class="panel">
 		<div class="previous"></div>
 		<div class="list splash">
-
-<!-- load block + js ? -->
-
+			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="300" height="100">
+				<param name="movie" value="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/poker_300x100.swf" />
+				<!--[if !IE]>-->
+				<object type="application/x-shockwave-flash" data="<?php print drupal_get_path("theme", "pbpoker") ?>/swf/promotion/poker_300x100.swf" width="300" height="100">
+				<!--<![endif]-->
+				<p></p>
+				<!--[if !IE]>-->
+				</object>
+				<!--<![endif]-->
+			</object>
 		</div>
+		<script language="JavaScript" type="text/javascript">
+		  function pb_open_tutorial() {
+		    if(typeof tb_show == 'function') {
+		      tb_show(undefined, '<?php print url('poker/help', array(
+		        'attributes' => array(
+              'class' => 'thickbox close',
+            ),
+            'fragment' => 'help-tutorial',
+            'query' => array("height" => 442, "width" => 603, 'keepThis' => TRUE, 'TB_iframe' => TRUE))) ?>', false);
+	      }
+		  }
+		</script>
 		<div class="next"></div>
 		<div class="clear"></div>
 	</div>
