db199873c76cd777836a4e3927ba0507daa4967f
root@drupal-chandan <root@drupal-chandan>
2010-02-17 08:56:17 +0100

    Force buddy approval regardless of site settings. Refs z2#169.

--- modules/os_poker/os_poker_forms.php
+++ modules/os_poker/os_poker_forms.php
@@ -71,6 +71,8 @@ function	os_poker_sign_up_form($form_state)
 	/* Invoke the user_relationship_invites_user() hook to ensure buddy relationship will be triggered
 		 upon submitting the sign up form */
 	$form = user_relationship_invites_user('register', $form, $account, NULL);
+	// Force buddy request to be approved
+	$form['relationship_invite_approve'] = array('#type' => 'value', '#value' => 'approve');
 				
 	$form["name"] = array(
 							'#type' => 'hidden',
