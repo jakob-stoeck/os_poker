9a14a6254fc358ae54ebb850ba3fadb7165db64a
Ulrike Uhlig <u@curlybracket.net>
2010-02-25 15:34:39 +0100

    adding static content in german to tourney info page. modifying the css a little bit to take into account the class on body. refs z2#179

--- modules/os_poker/tourney_info.tpl.php
+++ modules/os_poker/tourney_info.tpl.php
@@ -1,1 +1,58 @@
-TOURNEY INFO PLACEHOLDER !
+				<div id="tourney-teaser">
+					<h2><span>Spielen Sie mit unseren Playmates</span></h2>
+				</div>
+				<div id="middle-content">
+					<div id="middle-content-left">
+						<div class="banner-box">
+							<a href="#" id="banner-tourney"><!-- --></a>
+						</div>
+						<div class="banner-box">
+							<a href="#" id="banner-millions"><!-- --></a>
+						</div>
+						<div class="banner-box" id="become-member-box">
+							<h3><span>Noch kein Mitglied?</span></h3>
+							<p>Hol dir jetzt den Nervenkitzel, in High Stakes Partien und Turnieren um Millionen zu zocken, völlig ohne Risiko. Melde dich jetzt kostenlos an und sichere Dir $ 1.000 Chips Startguthaben!</p>
+							<p class="poker_submit">Jetzt registrieren</p>
+						</div>
+					</div>
+
+					<div id="middle-content-right">
+						<div class="tourney-infobox" id="prize-box">
+							<h3>Die Gewinne:</h3>
+							<ul>
+								<li>1. Preis: Besuch eines Playboy-Shootings</li>
+								<li>2. Preis: Lebenslanges Playboy-Abo</li>
+								<li>3. Preis: Lebenslanges CyberClub-Abo</li>
+							</ul>
+							<p>» zusätzlich 1 Million Chips Preispool!!!</p>
+						</div>
+						<div class="tourney-infobox" id="participation-box">
+							<h3>Wie kann ich teilnehmen?</h3>
+							<ul>
+								<li>- Jetzt kostenlos anmelden und 1.000 Chips Startguthaben sichern<br />
+								&nbsp;&nbsp; Nur noch Dein Profil ausfüllen und fertig! So einfach ist das...</li>
+								<li>- Das Turnier findest Du in der <a href="#">Lobby</a> unter "Turniere".<br />
+								&nbsp;&nbsp; Ab dem 02. März kannst Du dich dort für das Turnier registrieren.</li>
+								<li>- Noch nie gepokert? <a href="#">Hier</a> geht's zum Video-Tutorial mit Bunny Michaela.</li>
+							</ul>
+						</div>
+						<div class="tourney-infobox" id="date-box">
+							<h3>Ohne Buy-in zum Millionär!</h3>
+							<p><strong>Jeden ersten Sonntag im Monat ein Preispool mit 1 Million Chips, Nervenkitzel garantiert!</strong></p>
+							<p>Nächste Termine: <br />
+							07. März, 04. April, 02. Mai, 06. Juni, 04. Juli - Startzeit: 19:00 Uhr
+							</p>
+						</div>
+						<div class="tourney-infobox" id="tos-box">
+							<h3>Teilnahmebedingungen:</h3>
+							<ul>
+								<li>- Jeder Teilnehmer muss das 18. Lebensjahr vollendet haben.</li>
+								<li>- Die Teilnahme ist kostenlos!</li>
+								<li>- Die Preise sind nicht übertragbar oder in bar auszahlbar.</li>
+								<li>- Bei s#ömtlichen Einsätzen handelt es sich um virtuelles Spielgeld<br />
+								&nbsp;&nbsp; ohne Gegenwert.</li>
+								<li>- Wir bitten um Beachtung unserer <a href="#">Nutzungsbedingungen</a>.</li>
+							</ul>
+						</div>
+				    </div>
+				  </div>
