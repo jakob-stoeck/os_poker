65b7cf7b553ea80ba518b9904037ee33e7373abf
Jean Valjean <jean@valjean.com>
2010-03-11 11:17:50 +0100

    imported template for imprint thickbox ref: p4f#28

---
+++ themes/p4f/imprint.en.tpl.php
@@ -0,0 +1,108 @@
+<div id="ContainerContentHelp" class="tabs-window">
+	<a href="javascript:void(0);" onclick="javascript:parent.tb_remove();" class="LayerClose">&nbsp;</a>
+	<div class="tabs">
+		<ul class="tabs primary">
+			<li><a href="#help-imprint">Imprint</a></li>
+			<li><a href="#help-terms-of-service">Terms of Service</a></li>
+		</ul>
+	</div>
+
+	<div id="help-imprint" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Imprint</h4>
+		<p><strong>Playwest Ltd.</strong></p>
+		<p class="adress">69 Great Hampton Street Birmingham,<br />
+		B 18 6EW, UK<br />
+		Director: André Pass</p>
+
+
+		<?php //print l(t("Sources"), "<front>"); ?>
+		<a style="color: #6C6C6C" href="#">Drupal source code</a>
+	</div>
+	<div id="help-terms-of-service" class="helpbox">
+		<h4 class="helpbox-title block_title_bar">Terms of service</h4>
+		<p>PLEASE  READ THIS TERMS OF USE POLICY CAREFULLY. WHEN YOU USE ANY ONLINE GAME,  SERVICE or WEBSITE FROM ADUNIQUE Interactive GmbH. YOU ACKNOWLEDGE THAT  YOU HAVE READ, UNDERSTOOD, AND AGREE TO BE BOUND BY THESE TERMS. </p>
+
+		<h5>General</h5>
+		<p>Thank you for visiting this website ("Playboypoker.de"). This Website is owned by Playwest Ltd. (PW).  This Terms of Use Policy ("POLICY") governs your use of this Website.  This Website and all intellectual and other rights thereto are owned by  PW. With the exception of applications commonly known as Web Browser  software, or other applications formally approved by PW in writing,  you agree not to use any software, program, application or any other  device to access or log on to PW computer systems, Website or  proprietary software or to automate the process of obtaining,  downloading, transferring or transmitting any content, information or  quotes to or from PW computer systems, Website or proprietary  software. PW is not responsible for, and makes no warranties  regarding, the access, speed or availability of the Internet in general  or the Website in particular. You agree that you will not: (a) use any  robot, spider, other automatic device or manual process to monitor or  copy our web pages or the content contained herein without PW’s prior  written permission; (b) use any device, software or routine to  interfere or attempt to interfere with the proper working of the  Website or any transaction being conducted on either; (c) take any  action that imposes an unreasonable or disproportionately large load on  PW’s infrastructure; or (d) reverse engineer, reverse assemble or  otherwise attempt to discover any source code. You agree that you will  not copy, reproduce, alter, modify, create derivative works, or  publicly display any content from the Website without PW’s prior  written permission. You may not frame or utilize framing techniques to  enclose any trademark, logo, or other proprietary information  (including images, text, page layout, or form) of PW without the prior  written consent of PW. </p>
+		<h5>Revisions to this POLICY
+    	<p>In its sole discretion, PW may revise this POLICY at any time, effective when posted to this Website. </p>
+
+		<h5>Your Log in Account </h5>
+		<p>In order to play you may  establish a log in account ("Your Account"). You agree that it is your  sole responsibility to maintain the security of any password you may be  assigned to access Your Account. You agree to provide only accurate  information in setting up Your Account. Information you provide in  setting up Your Account may be collected, stored, and used as provided  in the Privacy Policy located on another page of this Website. You  should refer to the Privacy Policy. </p>
+
+		<h5>Compliance with All Laws </h5>
+    	<p>The Game, the  Service, Your Account, and this Website may not be used in connection  with criminal or civil violations of local, state, federal, or  international laws, treaties, regulations, or other governmental  requirements. Such violations include, but are not limited to, theft or  infringement of copyrights, trademarks or trade secrets, fraud,  forgery, theft or misappropriation of funds, libel and defamation. PW  may involve, and will cooperate with, law enforcement if criminal  activity is suspected. </p>
+
+		<h5>Termination of Your Account </h5>
+    	<p>PW reserves the  right to terminate without notice Your Account for any reason, such as  your violation of this POLICY or for no reason in PW’s sole  discretion. PW’s failure to enforce this POLICY, for whatever reason,  shall not be construed as waiver of our right to do so at any time in  the future. </p>
+
+		<h5>User Conduct </h5>
+    	<p>You understand that except for  advertising programs offered by us on the Website, the Service and the  Website are available for your personal, non-commercial use only. You  represent, warrant and agree that no materials of any kind submitted  through Your Account or otherwise posted, transmitted, or shared by you  on or through the Service will violate or infringe upon the rights of  any third party, including copyright, trademark, privacy, publicity or  other personal or proprietary rights; or contain libellous, defamatory  or otherwise unlawful material. </p>
+
+		<h5>In addition, you agree not to use the Service or the Website to: </h5>
+		<ol class="helpbox-list">
+			<li>register for more than one (1) user account, register for a user  account on behalf of an individual other than yourself, or register for  a user account on behalf of any group or entity;</li>
+			<li>harvest or collect email addresses or other contact information of  other users from the Service or the Website by electronic or other  means for the purposes of sending unsolicited emails or other  unsolicited communications;</li>
+			<li>use the Service or the Website in any unlawful manner or in any other  manner that could damage, disable, overburden or impair the Website;</li>
+			<li>use automated scripts to collect information from or otherwise interact with the Service or the Website;</li>
+			<li>upload, post, transmit, share, store or otherwise make available any  content that we deem to be harmful, threatening, unlawful, defamatory,  infringing, abusive, inflammatory, harassing, vulgar, obscene,  fraudulent, invasive of privacy or publicity rights, hateful, or  racially, ethnically or otherwise objectionable;</li>
+			<li>impersonate any person or entity, or falsely state or otherwise  misrepresent yourself, your age or your affiliation with any person or  entity;</li>
+			<li>upload, post, transmit, share or otherwise make available any  unsolicited or unauthorized advertising, solicitations, promotional  materials, "junk mail," "spam," "chain letters," "pyramid schemes," or  any other form of solicitation;</li>
+			<li>upload, post, transmit, share, store or otherwise make publicly  available on the Website any private information of any third party,  including, addresses, phone numbers, email addresses, Social Security  numbers and credit card numbers;</li>
+			<li>transmit or facilitate distribution of content that is harmful,  abusive, racially or ethnically offensive, vulgar, sexually explicit,  defamatory, infringing, invasive of personal privacy or publicity  rights, or in a reasonable person's view, objectionable. Hate speech is  not tolerated.</li>
+			<li>solicit personal information from anyone under 18 or solicit passwords  or personally identifying information for commercial or unlawful  purposes;</li>
+			<li>disrupt the flow of chat in chat rooms or in game with vulgar language,  abusiveness, hitting the return key repeatedly or inputting large  images so the screen goes by too fast to read, use of excessive  shouting [all caps] in an attempt to disturb other users, "spamming" or  flooding [posting repetitive text].</li>
+			<li>attempt to get a password, account information, or other private information from anyone else on the Services;</li>
+			<li>upload, post, transmit, share or otherwise make available any material  that contains software viruses or any other computer code, files or  programs designed to interrupt, destroy or limit the functionality of  any computer software or hardware or telecommunications equipment;</li>
+			<li>harass, threaten, or do anything else to another player that is  unwanted, such as repeatedly sending unwanted chat messages or making  personal attacks or statements about race, sexual orientation,  religion, heritage, etc;</li>
+			<li>upload, post, transmit, share, store or otherwise make available  content that would constitute, encourage or provide instructions for a  criminal offense, violate the rights of any party, or that would  otherwise create liability or violate any local, state, national or  international law;</li>
+			<li>promote or encourage any illegal activity including hacking, cracking or distribution of counterfeit software;</li>
+			<li>use and communicate exploits to gain unfair advantage in a game;</li>
+			<li>use or attempt to use another's account, service or system without  authorization from PW, or create a false identity on the Service or  the Website;</li>
+	  </ul>
+
+		<h5>Illegal Use </h5>
+    	<p>The transmission, distribution, or  storage of any data or material in violation of any applicable law or  regulation is prohibited. This includes, but is not limited to,  material or data which:</p>
+		<ul class="helpbox-list">
+			<li>infringes or otherwise violates any copyright, trademark, trade secret, or other intellectual property right;</li>
+			<li>violates import or export control laws or regulations;</li>
+			<li>violates any third party's confidentiality, privacy, publicity, or other rights;</li>
+			<li>constitutes use or dissemination of child pornography; or</li>
+			<li>is otherwise illegal or unlawful.</li>
+  		</ul>
+
+		<h5>Abuse</h5>
+		<p>The following general actions are considered "abuse" and are strictly prohibited:</p>
+		<ul class="helpbox-list">
+			<li>using this Website to transmit material PW believe to be obscene or illegal;</li>
+			<li>forging message headers or identity information, or taking any action  with the intent of bypassing restrictions or limits on access to a  specific service or Website. This prohibition does not restrict the  legitimate non-commercial use of pseudonymous or anonymous services; and</li>
+			<li>falsifying identity or contact information.</li>
+		</ul>
+
+		<h5>Security</h5>
+		<p>You must configure your system in a  secure manner. Should your system be exploited by unauthorized persons,  you are responsible for both reporting the violation (where  applicable), and fixing the exploited system. For instance, should the  security of a mail server be compromised to distribute unsolicited  emails, you are responsible for immediately re-configuring the system  to prevent further unauthorized use. You are prohibited from  interfering or attempting to interfere with services (also referred to  as Denial of Service Attacks), whether intentionally or through  neglect, of any other user, host, or network. The prevention of  "unintentional attacks", such as infection and subsequent propagation  of computer viruses, is your responsibility. Anti-virus software should  be installed on every system which connects to the website. </p>
+
+		<h5>E-Mail</h5>
+    	<p>You are strictly prohibited from engaging, whether intentionally or through neglect, in any of the following activities:</p>
+		<ol class="helobox-list">
+			<li>sending unsolicited mass or commercial e-mail (also referred to as "spamming") for any purpose whatsoever;</li>
+			<li>altering or faking e-mail header information to misdirect or otherwise  "spoof" another party for the purpose of evading detection or any other  purposes; and</li>
+			<li>configuring any e-mail server in such a way that it will accept third  party e-mails for forwarding (commonly known as an "open mail relay").  If a Website has roaming users who wish to use a common mail server,  the mail server must be configured to require some form of user  identification and authorization.</li>
+		</ol>
+		<p>Users that send mass or commercial e-mail are required to maintain  complete and accurate records of all e-mail activity for a reasonable  time, and to immediately provide PW with such records upon request of  PW. This may include requests for database verification of a contact's  removal. </p>
+
+		<h5>Additional Prohibitions </h5>
+		<p>In addition to the  prohibitions against Spam as detaily, privacy practices or other policies of or contained in the  Third Party Websites or the Third Party Applications, Software or  Content. Inclusion of, linking to or permitting the use or installation  of any Third Party Website or any Third Party Applications, Software or  Content does not imply approval or endorsement thereof by us. If you  decide to leave the Website and access the Third Party Websites or to  use or install any Third Party Applications, Software or Content, you  do so at your own risk and you should be aware that our terms and  policies no longer govern. You should review the applicable terms and  policies, including privacy and data gathering practices, of any  Website to which you navigate from the Website or relating to any  applications you use or install from the Website. </p>
+		<h5>Injunctive Relief </h5>
+		<p>You agree that your  violation of this POLICY may result in immediate and irreparable harm  to PW for which it may not be compensated adequately by a monetary  award. In the event of your actual or threatened violation of this  POLICY, you agree that PW shall be entitled to an injunction or other  equitable relief, in addition to such other relief to which such  parties may be entitled, including monetary relief, appropriately  enjoining or restraining conduct in violation of this POLICY, without  having to post bond or other security. </p>
+
+		<h5>Disclaimer of Warranties </h5>
+    PW HEREBY DISCLAIMS,  TO THE FULLEST EXTENT ALLOWED BY APPLICABLE LAW, ALL WARRANTIES,  INCLUDING THE IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE,  TITLE, NON-INFRINGEMENT, AND MERCHANTABILITY, AND ALL OTHER WARRANTIES  NOT EXPRESSLY PROVIDED IN THIS PRIVACY POLICY. </p>
+		<p>The Website and the Service may be temporarily unavailable from time  to time for maintenance or other reasons. PW assumes no responsibility  for any error, omission, interruption, deletion, defect, delay in  operation or transmission, communications line failure, theft or  desproviders, computer or mobile phone equipment, software,  failure of email or players on account of technical problems or traffic  congestion on the Internet or at any Website or combination thereof,  including injury or damage to User's or to any other person's computer,  mobile phone, or other hardware or software, related to or resulting  from using or downloading materials in connection with the Web and/or  in connection with the Service, including any Software from PW. Under  no circumstances will PW be responsible for any loss or damage,  including any loss or damage to any User content or personal injury or  death, resulting from anyone's use of the Website or the Service, any  User content or Third Party Applications, Software or Content posted on  or through the Website or the Service or transmitted to Users, or any  interactions between users of the Website, whether online or offline. </p>
+
+		<h5>Entire Agreement </h5>
+	    This POLICY sets forth the entire agreement between you and PW regarding the subject matter of this POLICY. </p>
+		<p><strong>Legally the German version of these terms and conditions are imperative.</strong></p>
+
+	</div>
+</div>
