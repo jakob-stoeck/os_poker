346247c7ae8e2093afaf4efa3d2392238797b846
Pierre Buyle <pierre@buyle.org>
2010-03-31 18:02:56 +0200

    Use translated texte for front page elements (refs #90)

--- themes/p4f/page-front-banners.tpl.php
+++ themes/p4f/page-front-banners.tpl.php
@@ -1,19 +1,14 @@
 <?php // Added by n4mu to simplify (i hope)
 $cfg_theme_path = drupal_get_path("theme", "p4f")?>
 
-<? // original stuff: commented out ?>
-          <!--<div id="page-front-banners">
-            <?php //foreach($banners as $banner) { print $banner; } ?>
-          </div> -->
-
 <? // Static home page things ?>
 <div class="white_boxes">
 	<h2 class="title_bar">Preise</h2>
 	<div class="fleft prize_box">
 		<img class="fleft" src="<? print $cfg_theme_path; ?>/images/prize_hits.png" />
 		<div class="prize_txt">
-			<h3>Tagespreis</h3>
-			<p>Lorem text lol bonjour</p>
+			<h3><?php print t('Daily Prize') ?></h3>
+			<p><?php print t('Description of the Daily Prize for front page.') ?></p>
 		</div>
 	</div>
 	<div class="fright prize_box">
@@ -21,8 +16,8 @@ $cfg_theme_path = drupal_get_path("theme", "p4f")?>
 			<img src="<? print $cfg_theme_path; ?>/images/prize_cdpack.png" />
 		</div>
 		<div class="prize_txt">
-			<h3>Wochenpreis</h3>
-			<p>Lorem text lol bonjour</p>
+			<h3><?php print t('Weekly Prize') ?></h3>
+			<p><?php print t('Description of the Weekly Prize for front page.') ?></p>
 		</div>
 	</div>
 	<div class="fleft prize_box">
@@ -30,8 +25,8 @@ $cfg_theme_path = drupal_get_path("theme", "p4f")?>
 			<img src="<? print $cfg_theme_path; ?>/images/prize_fashion.png" />
 		</div>
 		<div class="prize_txt">
-			<h3>Monatpreis</h3>
-			<p>Lorem text lol bonjour</p>
+			<h3><?php print t('Monthly Prize') ?></h3>
+			<p><?php print t('Description of the Monthly Prize for front page.') ?></p>
 		</div>
 	</div>
 	<div class="fright prize_box">
@@ -39,8 +34,8 @@ $cfg_theme_path = drupal_get_path("theme", "p4f")?>
 			<img src="<? print $cfg_theme_path; ?>/images/prize_tbo.png" />
 		</div>
 		<div class="prize_txt">
-			<h3>Jahrespreis 2010</h3>
-			<p>Lorem text lol bonjour</p>
+			<h3><?php print t('Yearly Prize') ?> 2010</h3>
+			<p><?php print t('Description of the Yearly Prize for front page.') ?></p>
 		</div>
 	</div>
 	<div class="clear"></div>
@@ -51,35 +46,31 @@ $cfg_theme_path = drupal_get_path("theme", "p4f")?>
 	<div class="games_hp">
 		<? // this should be dynamic, 1 out of 7 games ?>
 		<img src="<? print $cfg_theme_path; ?>/images/games_fashion_poker.png" alt=""/>
-		<h3>Fashion Poker</h3>
-		<p>This text will be the "teasertext", that should be about 2 lines according to the do. On this page
-		it will but cut to about 4...</p>
+		<h3><?php print t('Fashion Poker')?></h3>
+		<p><?php print t('Description of the Fashion Poker game for front page.') ?></p>
 		<a class="submit_get" href="#">Jetzt Spielen</a>
 	</div>
 	<div class="games_hp">
 		<? // this should be dynamic, 1 out of 7 games ?>
 		<img src="<? print $cfg_theme_path; ?>/images/games_cartingo.png" alt=""/>
-		<h3>Cartingo</h3>
-		<p>This text will be the "teasertext", that should be about 2 lines according to the do. On this page
-		it will but cut to about 4...</p>
+		<h3><?php print t('Cartingo') ?></h3>
+		<p><?php print t('Description the Cartingo game for front page.') ?></p>
 		<a class="submit_get" href="#">Jetzt Spielen</a>
 	</div>
 	<div class="games_hp">
 		<? // this should be dynamic, 1 out of 7 games ?>
 		<img src="<? print $cfg_theme_path; ?>/images/games_nextdj.png" alt=""/>
-		<h3>Be the next DJ</h3>
-		<p>This text will be the "teasertext", that should be about 2 lines according to the do. On this page
-		it will but cut to about 4...</p>
+		<h3><?php print t('Be the next DJ') ?></h3>
+		<p><?php print t('Description of the Be the next DJ game for front page.') ?></p>
 		<a class="submit_get" href="#">Jetzt Spielen</a>
 	</div>
 	<div class="games_hp">
 		<? // this should be dynamic, 1 out of 7 games ?>
 		<img src="<? print $cfg_theme_path; ?>/images/games_fashion_quiz.png" alt=""/>
-		<h3>Fashion Quiz</h3>
-		<p>This text will be the "teasertext", that should be about 2 lines according to the do. On this page
-		it will but cut to about 4...</p>
+		<h3><?php print t('Fashion Quiz')?></h3>
+		<p><?php print t('Description of the Fashion Quiz game for front page.') ?></p>
 		<a class="submit_get" href="#">Jetzt Spielen</a>
 	</div>
 	<div class="clear"></div>
 </div>
-<p class="seo_text">Du interessierst dich für Fashion und online Games bei denen Du wertvolle Gewinne gratis erspielen kannst, dann bist Du bei Play4fashion genau auf der richtigen Spielfläche. Wir bieten online Poker, Flipper, Air Hockey, Lady Gaga Musik Spiele, Quiz und mehr. Gewinne gratis online Preise aus der Fashion Welt von NewYorker. Registriere Dich jetzt kostenlos und gewinne noch heute.</p>
+<p class="seo_text"<?php print ('SEO Test for the front page.') ;?></p>
