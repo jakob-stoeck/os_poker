87b9a922a5edb73452cc2180d2f3f7743d741184
Pierre Buyle <pierre@buyle.org>
2010-04-08 16:57:56 +0200

    Lobby Improvments (refs z2#253)  * Rename modules/os_poker/news.tpl.php to modules/os_poker/os-poker-home-news.tpl.php  * Overrides os-poker-home-news.tpl.php and os-poker-home-promotion.tpl.php in pbpoker theme  * Change ''news'' and ''promotion'' banners content to ''Invite Friends'' and ''Video Tutorial''  * Fix ''backward compatibility'' to only apply thickboxing to secondary-links menu items to poker/* pages

--- modules/os_poker/js/os_poker.menu.js
+++ modules/os_poker/js/os_poker.menu.js
@@ -38,7 +38,7 @@ function	os_poker_brutal_logout()
 function	os_poker_init_menu()
 {
 	//Prepare menu for Thickbox
-	$("#block-menu-secondary-links a, a#forgot_password, #block-menu-menu-messages-links a, #block-menu-menu-end-links .first a").each(function() {
+	$("#block-menu-secondary-links a[href*=poker/], a#forgot_password, #block-menu-menu-messages-links a, #block-menu-menu-end-links .first a").each(function() {
 		var tg = $(this).attr("href");
 		
 		$(this).attr("href", tg + "&keepThis=true&TB_iframe=true");
