991e127792728747b5974bafa66cae007a3ffd82
Ulrike Uhlig <u@curlybracket.net>
2010-04-13 19:32:03 +0200

    tourneypage refs #264

--- themes/pbpoker/os-poker-tourney-info.tpl.php
+++ themes/pbpoker/os-poker-tourney-info.tpl.php
@@ -34,48 +34,25 @@ Nervenkitzel garantiert! Los geht's um 19:00 Uhr.</p>
 							<p>Hier trennt sich die Spreu vom Weizen, denn wer kann sich schon ein Buy-in von 10.000 Chips leisten? Die Turniere des Gentleman‘s Pokerclub finden alle 2 Wochen Sonntags um 19:00 Uhr statt. </p>
 						</div>
 					</div>
-
-              <a class="thickbox" href="<?php print url("poker/help", array('query' => 'height=442&width=603&keepThis=true&TB_iframe=true&', 'fragment' => 'help-tutorial')); ?>">
-                Tutorial
-              </a>
 					<div id="middle-content-right">
 						<div class="tourney-infobox" id="prize-box">
-							<h3>Die Gewinne:</h3>
+							<h3>Unsere Preise</h3>
 							<ul>
-								<li>1. Preis: Besuch eines Playboy-Shootings</li>
-								<li>2. Preis: Lebenslanges Playboy-Abo</li>
-								<li>3. Preis: Lebenslanges CyberClub-Abo</li>
+								<li>- Teilnahme am Fotoshooting</li>
+								<li>- Playboy Magazin Abo</li>
+								<li>- Playboy Cyberclub Abo</li>
+								<li>- Kartenspiele und Chips</li>
+								<li>- u.v.m.</li>
 							</ul>
-							<p>» zusätzlich 1 Million Chips Preispool!!!</p>
 						</div>
 						<div class="tourney-infobox" id="participation-box">
 							<h3>Wie kann ich teilnehmen?</h3>
-							<ul>
-								<li>- Jetzt kostenlos anmelden und 1.000 Chips Startguthaben sichern<br />
-								&nbsp;&nbsp; Nur noch Dein Profil ausfüllen und fertig! So einfach ist das...</li>
-								<li>- Das Turnier findest Du in der <?php print l("Lobby", ""); ?> unter "Turniere".<br />
-								&nbsp;&nbsp; Ab dem 02. März kannst Du dich dort für das Turnier registrieren.</li>
-								<li>- Noch nie gepokert? <?php print l("Hier", "poker/help/&initcall_show_tutorial_tab=true&height=442&width=603&keepThis=true&TB_iframe=true", array('attributes' => array('class' => "thickbox"))); ?> geht's zum Video-Tutorial mit Bunny Michaela.</li>
-							</ul>
+							<p>Jetzt kostenlos anmelden und <strong>$1.000 Chips</strong> Startguthaben sichern. Nur noch Dein Profil ausfüllen und fertig! So einfach ist das... Die Turniere findest du in der Lobby unter "Turniere", hier kannst du dich registrieren.</p>
 						</div>
-						<div class="tourney-infobox" id="date-box">
-							<h3>Ohne Buy-in zum Millionär!</h3>
-							<p><strong>Jeden ersten Sonntag im Monat ein Preispool mit 1 Million Chips, Nervenkitzel garantiert!</strong></p>
-							<p>Nächste Termine: <br />
-							07. März, 04. April, 02. Mai, 06. Juni, 04. Juli - Startzeit: 19:00 Uhr
-							</p>
-						</div>
-						<div class="tourney-infobox" id="tos-box">
-							<h3>Teilnahmebedingungen:</h3>
-							<ul>
-								<li>- Jeder Teilnehmer muss das 18. Lebensjahr vollendet haben.</li>
-								<li>- Die Teilnahme ist kostenlos!</li>
-								<li>- Die Preise sind nicht übertragbar oder in bar auszahlbar.</li>
-								<li>- Bei sämtlichen Einsätzen handelt es sich um virtuelles Spielgeld<br />
-								&nbsp;&nbsp; ohne Gegenwert.</li>
-								<li>- Wir bitten um Beachtung unserer <?php print l("Nutzungsbedingungen", "poker/tos", array('attributes' => array('class' => 'thickbox'),
-'query' => array("height" => 442, "width" => 603, 'keepThis' => TRUE, 'TB_iframe' => TRUE),)); ?>.</li>
-							</ul>
+						<div class="tourney-infobox" id="video-box">
+							<h3>Noch nie gepokert?</h3>
+							<p id="gototuto">Hier geht's zum Videotutorial mit Bunny Michaela.</p>
+							<div class="poker_submit"><div class="pre"></div><div class="label"><?php print l("zum Tutorial", "poker/help/&initcall_show_tutorial_tab=true&height=442&width=603&keepThis=true&TB_iframe=true"); ?></div></div>
 						</div>
 				    </div>
 				  </div>
