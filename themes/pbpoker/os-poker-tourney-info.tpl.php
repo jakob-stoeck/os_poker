f90ec54046738af6a6c26ee2ba488aa49cea0532
Ulrike Uhlig <u@curlybracket.net>
2010-04-15 15:09:58 +0200

    new text for pokerpage refs z2#262 z2#264

--- themes/pbpoker/os-poker-tourney-info.tpl.php
+++ themes/pbpoker/os-poker-tourney-info.tpl.php
@@ -7,31 +7,34 @@
 					<div id="middle-content-left">
 						<div class="infobox" id="poker-masters">
 							<h3>Playboy Poker Masters</h3>
-							<h4>Playboy Poker Masters</h4>
-							<p>Das grosse Playboypoker Turnier um wechselnde Sachpreise.<br />
-							   Dieses Turnier hat kein Buy-in und findet alle 2 Monate zu festgelegten Terminen statt.</p>
+						    <p>Die Turniere der <strong>Playboy Poker Masters</strong> darf man sich einfach nicht entgehen lassen: Exklusive Preise von Playboy für die Gewinner, besser kann man die Zeit am Sonntagabend kaum nutzen!</p>
+							<p>Die Playboy Poker Masters finden alle 2 Monate statt.<br />
+							<strong>Nächster Termin: Sonntag, 25. April um 19:00 Uhr</strong></p>
 						</div>
 						<div class="infobox" id="bunny-hunter">
 							<h3>Bunny Hunter</h3>
-							<h4>Bunny Hunter</h4>
-							<p>Die Hasen-Jagd-Saison ist eröffnet!<br />
-							Die Besten teilen sich den Preispool aber wer dem Bunny den letzten Chip aus der Tasche zieht, kann den Bunny Hunter Special Preis als Trophäe mit nach Hause nehmen.</p>
+							<h4>Die Hasen-Jagd-Saison ist eröffnet</h4>
+							<p>Die Besten teilen sich den Preispool aber wer dem Bunny den letzten Chip aus der Tasche zieht, kann den Bunny Hunter Special Preis als Trophäe mit nach Hause nehmen.</p>
+							<p><strong>Jeden dritten Sonntag im Monat um 19:00 Uhr.</strong></p>
 						</div>
 						<div class="infobox" id="millions">
 							<h3>Monthly Millions</h3>
 							<h4>Monthly Millions</h4>
-							<p>Jeden ersten Sonntag im Monat ein Preispool mit 1 Million Chips,
-Nervenkitzel garantiert! Los geht's um 19:00 Uhr.</p>
+							<p>Der Sonntag ist auf Playboy Poker garantiert kein Ruhetag!</p>
+							<p>Jeden ersten Sonntag im Monat ein Preispool mit <strong>1 Million Chips</strong>, Nervenkitzel garantiert bei den <strong>Monthly Millions</strong>!</p>
+							<p>Los geht's immer <strong>um 19:00 Uhr.</strong></p>
 						</div>
 						<div class="infobox" id="after-work">
 							<h3>After Work Poker</h3>
 							<h4>After Work Poker</h4>
-							<p>Spät ins Bett gehen nur weil man ein Pokerturnier spielen will, muss unter der Woche niemand mehr. Auf Playboy Poker jeden Donnerstag um 18:30 Uhr ganz entspannt Afterwork Poker spielen!</p>
+							<p>Spät ins Bett gehen nur weil man ein Pokerturnier spielen will, muss unter der Woche niemand mehr.</p>
+							<p>Auf Playboy Poker <strong>jeden Donnerstag um 18:30 Uhr</strong> ganz entspannt <strong>Afterwork Poker</strong> spielen!</p>
 						</div>
 						<div class="infobox" id="gentlemen">
 							<h3>Gentleman's Pokerclub</h3>
 							<h4>Gentleman's Pokerclub</h4>
-							<p>Hier trennt sich die Spreu vom Weizen, denn wer kann sich schon ein Buy-in von 10.000 Chips leisten? Die Turniere des Gentleman‘s Pokerclub finden alle 2 Wochen Sonntags um 19:00 Uhr statt. </p>
+							<p>Hier trennt sich die Spreu vom Weizen, denn wer kann sich schon ein Buy-in von 10.000 Chips leisten?</p>
+							<p>Die Turniere des Gentleman‘s Pokerclub finden <strong>alle 2 Wochen Sonntags um 19:00 Uhr statt</strong>. </p>
 						</div>
 					</div>
 					<div id="middle-content-right">
