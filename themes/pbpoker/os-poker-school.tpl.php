004e7a677872be8f79d58a6226b26962a7247a23
Ulrike Uhlig <u@curlybracket.net>
2010-04-15 13:44:44 +0200

    skelelton for pokerschool page refs z2#262

--- themes/pbpoker/os-poker-school.tpl.php
+++ themes/pbpoker/os-poker-school.tpl.php
@@ -12,7 +12,39 @@
 				</div>
 				<div id="middle-content">
 					<div id="middle-content-left">
+						<div class="infobox" id="cards">
+							<h3>Pokerblätter</h3>
+							<p>Vom Paar bis zum Royal Flush, gewinnen kann man mit jedem Blatt. Um zu wissen, wie gut Deine Karten wirklich sind, musst du nur 9 Pokerblätter kennen. Klicke auf "weiter" um mehr kennenzulernen.</p>
+						</div>
+						<div class="infobox" id="rules">
+							<h3>Hold'Em Regeln</h3>
+							<p></p>
+						</div>
+						<div class="infobox" id="tips">
+							<h3>Tips</h3>
+							<p></p>
+						</div>
+						<div class="infobox" id="guidelines">
+							<h3>Richtlininien</h3>
+							<p></p>
+						</div>
 					</div>
 					<div id="middle-content-right">
+						<div class="infobox" id="video-box">
+							<h3>Noch nie gepokert?</h3>
+							<p id="gototuto">Hier geht's zum Videotutorial mit Bunny Michaela.</p>
+							<div class="poker_submit"><div class="pre"></div><div class="label"><?php print l("zum Tutorial", "poker/help/&initcall_show_tutorial_tab=true&height=442&width=603&keepThis=true&TB_iframe=true"); ?></div></div>
+						</div>
+						<div class="infobox" id="quicklinks">
+							<h3>Quicklinks</h3>
+							<ul>
+								<li>» <a href="">Tutorial</a></li>
+								<li>» <a href="">Hold'em Regeln</a></li>
+								<li>» <a href="">Pokerblätter</a></li>
+								<li>» <a href="">Tipps</a></li>
+							</ul>
+						</div>
+						<div class="teaser" id="tourneys">
+						</div>
 					</div>
 				</div>
