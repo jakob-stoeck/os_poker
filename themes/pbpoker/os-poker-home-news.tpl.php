ffd790923a5066dffd991aae7de9428d1aec3096
root@localhost <root@localhost>
2010-04-22 18:16:33 +0200

    change promotion banner

--- themes/pbpoker/os-poker-home-news.tpl.php
+++ themes/pbpoker/os-poker-home-news.tpl.php
@@ -3,14 +3,13 @@
 		<div class="previous"></div>
       <div class="list splash">
         <div id="home_promotion_1-banner">
-          <div class="header block_title_bar block_title_text">Freunde einladen</div>
-          <?php print theme('image', drupal_get_path('theme', 'pbpoker') .'/images/home_promotion_1.jpg'); ?>
-          <p>Je mehr Freunde Du einlädst, umso mehr Chips bekommst Du von uns geschenkt!</p>
+          <div class="header block_title_bar block_title_text">Playboy Poker Masters</div>
+          <?php print theme('image', drupal_get_path('theme', 'pbpoker') .'/images/home_promotion_1b.jpg'); ?>
+          <p>Exklusive Preise von Playboy für die Gewinner! Nächster Termin: Sonntag, 25. April um 19:00 Uhr</p>
           <div id="home_promotion_1-button" class="poker_submit">
             <div class="pre">&nbsp;</div>
             <div class="label">
-              <a class="thickbox" href="<?php print url('poker/buddies/invite', array(
-                'query' => array("height" => 442, "width" => 603, 'keepThis' => TRUE, 'TB_iframe' => TRUE)))?>">Einladen</a>
+              <a class="banner" href="<?php print url('poker/pages/tourneyinfo')?>">mehr Infos</a>
             </div>
             <div class="user_login_clear"></div>
           </div>
