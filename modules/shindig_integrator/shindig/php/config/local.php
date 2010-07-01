44e0c329addd30e1f725dc6b9f141d4c64dc8344
Jakob Stoeck <kontakt@stoeck.it>
2010-07-01 15:23:14 +0200

    Added shinding configuration files. They need to be updated on another server.

---
+++ modules/shindig_integrator/shindig/php/config/local.php
@@ -0,0 +1,35 @@
+<?php
+
+    /**
+     * Configuration class.
+     */
+    class ShindigConfigException extends Exception {}
+
+      $shindigConfig = array(
+//      "gadget_server" => "http://poker.playboy.de/sites/all/modules/shindig_integrator/shindig/php",
+      "gadget_server" => "http://pbpoker-dev.stoeck.it/sites/all/modules/shindig_integrator/shindig/php",
+      "web_prefix" => "/sites/all/modules/shindig_integrator/shindig/php",
+      "default_js_prefix" => "/sites/all/modules/shindig_integrator/shindig/php/gadgets/js/",
+      "default_iframe_prefix" => "sites/all/modules/shindig_integrator/shindig/php/gadgets/ifr?",
+      "include_path" => "/usr/share/drupal6/includes/",
+      "settings_php" => "/usr/share/drupal6/sites/default/settings.php",
+      "person_service" => "ShindigIntegratorPeopleService",
+      "activity_service" => "ShindigIntegratorActivitiesService",
+      "app_data_service" => "ShindigIntegratorAppDataService",
+      "drupal_base_path" => "/",
+      "drupal_dir" => "/usr/share/drupal6",
+    );
+    $GLOBALS["shindigConfig"] = $shindigConfig;
+
+    class ShindigConfig {
+    static function get($key)
+    {					
+      global $shindigConfig;						
+     if (isset($shindigConfig[$key])) {
+       return $shindigConfig[$key];
+     }
+     else {
+       throw new ShindigConfigException("Invalid Config Key");
+     }
+    }
+  }
