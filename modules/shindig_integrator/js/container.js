ea5cb6034555b7d08609d062cfa7bdfcc03575dc
Christian Wiedemann <cw@christian.wiedemann@key-tec.de>
2010-07-20 13:27:27 +0200

    jquery extend update

--- modules/shindig_integrator/js/container.js
+++ modules/shindig_integrator/js/container.js
@@ -20,7 +20,7 @@
 /**
  * This class implements the basic OpenSocia container functionality, see the RPC service hooks in the init function as reference
  */
-var Container = Class.extend({
+var Container = $.extend({
 	
 	maxHeight: 4096,
 	
