3786b8fb3277176733fb107c52c88340595908d9
root <root@pokermania.stoeck.it>
2010-08-27 13:28:15 +0200

    pokermania design

---
+++ themes/pmpoker/.svn/text-base/buddies_search.tpl.php.svn-base
@@ -0,0 +1,11 @@
+<div id="buddies-search">
+   <div class="buddies-search-banner">
+   <h2><?php print t("Looking for other players?"); ?></h2>
+   <p><?php print t("Then you've come to the right place! Whether you're looking for someone nearby, nationwide or in a foreign country, we show you all Poker players at a glance."); ?></p>
+   </div>
+	<?php print($form); ?>
+
+   <p class="buddies-search-infos">
+   <?php print t("* Please note that you can only add players to your buddy list or send gifts when you are both playing at the same table."); ?>
+   </p>
+</div>
---
+++ themes/pmpoker/os-poker-promotion-and-events.tpl.php
@@ -0,0 +1,55 @@
+<?php
+//
+//    Copyright (C) 2009, 2010 Pokermania
+//    Copyright (C) 2010 OutFlop
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+?>
+				<div id="events-teaser" class="main-teaser">
+					<?php if (!user_is_logged_in()): ?>
+					<h2><a href="<?php print url("", array('fragment' => 'registration-window')); ?>"><?php print t('Register now'); ?></a></h2>
+					<?php endif; ?>
+				</div>
+				<div id="middle-content">
+					<div id="middle-content-left">
+						<div class="infobox" id="randy">
+							<h3>Randy Dandy</h3>
+							<p><strong>Freunde einladen und dafür täglich Chips kassieren!</strong></p>
+							<p>Unter allen Mitspielern, die mindestens 5 Freunde erfolgreich eingeladen haben, verlosen wir jeden Monat eine von dem aktuellen Cover-Girl signierte Ausgabe des Playboys.</p>
+						</div>
+						<div class="infobox" id="party">
+							<h3>Playboy Club-Tour 2010 - Party, Spass und Playmates</h3>
+							<p>Je besser du spielst, je höher du aufsteigst in der Poker-Hierarchie, desto grösser werden deine Chancen auf ein Treffen mit einem echten Playboy Bunny bei der Playboy Club-Tour 2010. </p>
+							<p>Der Gewinner wird Anfang November ermittelt.</p>
+						</div>
+						<div class="infobox" id="sitngochamp">
+							<h3>Sit & Go's Champion</h3>
+							<p>Lade deine Freunde ein und spiele mit ihnen die <strong>Playboy Poker Mini Turniere</strong>. Unter allen Spielern, die den Tisch regelmässig vollmachen, verlosen wir Bunny Pokerkoffer.</p>
+						</div>
+						<div class="infobox" id="limo">
+							<h3>Playboy Poker Stretchlimo</h3>
+							<p><strong>Die goldene Playboy Poker Stretchlimo</strong><br />Hole als erster sämtliche Auszeichnungen und gewinne für ein Wochenende die goldene Playboy Poker Stretchlimousine.</p>
+						</div>
+					</div>
+					<div id="middle-content-right">
+						<div class="infobox" id="spender-box">
+							<h3>Big Spender 2010</h3>
+							<p><strong>Big Spender 2010</strong><br />Spendiere deinen Mitspielern Drinks, Zigarren, oder ein GoGo Girl. Wer wird der<br /><strong>Big Spender 2010</strong>?</p>
+						</div>
+						<div class="teaser" id="schoolteaser">
+							<?php print l("Zur Pokerschule", "poker/pages/school"); ?>
+						</div>
+					</div>
+				</div>
