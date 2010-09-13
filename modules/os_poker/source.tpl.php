47069e560abcf58f5d4e52f96a070d6edb3b865a
Jakob Stoeck <kontakt@stoeck.it>
2010-09-13 17:05:18 +0200

    powered by pokermania changed

--- modules/os_poker/source.tpl.php
+++ modules/os_poker/source.tpl.php
@@ -1,13 +1,13 @@
 <div id="ContainerContentHelp" class="tabs-window">
 	<div id="help-sources" class="helpbox" style="color:#fff">
 		<h4 class="helpbox-title block_title_bar">Pokermania Software &amp; Services</h4>
+		<p>Die Pokermania GmbH <a href="http://www.pokermaniagmbh.com/" target="_top" style="color:#6c6c6c">www.pokermaniagmbh.com</a>, mit Sitz in Köln, ist ein Full Service Medien- und Marketingdienstleister ím Online Gaming Segment Poker und Mitglied im Adunique Network <a href="http://www.adunique.com/" target="_top" style="color:#6c6c6c">www.adunique.com</a>. Pokermania bietet insbesondere Services und Software Lösungen für den Online Betrieb von "Casual Poker". "Casual Poker" ist ein reines Unterhaltungsspiel mit virtuellem Spielgeld ohne Gegenwert.</p>
 		<p>Die folgenden Programme sind freie Software. Sie können sie unter den Bedingungen der GNU General Public License und der GNU Affero General Public Licence, wie von der Free Software Foundation veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren Version:</p>
 		<ul>
 			<li><a href="/os_poker/poker.zip">jpoker-1.0.16 und poker-network-1.7.5</a></li>
 			<li><a href="http://drupal.org/">Drupal 6</a></li>
 			<li>Drupal-Erweiterungen: $ git clone http://poker.playboy.de/sites/all/.git</li>
 		</ul>
-		<p>Die Pokermania GmbH <a href="http://www.pokermaniagmbh.com/" target="_top" style="color:#6c6c6c">www.pokermaniagmbh.com</a>, mit Sitz in Köln, ist ein Full Service Medien- und Marketingdienstleister ím Online Gaming Segment Poker und Mitglied im Adunique Network <a href="http://www.adunique.com/" target="_top" style="color:#6c6c6c">www.adunique.com</a>. Pokermania bietet insbesondere Services und Software Lösungen für den Online Betrieb von "Casual Poker". "Casual Poker" ist ein reines Unterhaltungsspiel mit virtuellem Spielgeld ohne Gegenwert.</p>
 		<p>Copyright © 2010 Pokermania GmbH, Germany</p>
 	</div>
 </div>
